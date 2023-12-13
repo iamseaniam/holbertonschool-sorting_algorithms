cda995dfb2a1334fe8e46ded1ead4a0c07c47c16
iamseaniam <7222@holbertonstudents.com>
2023-12-13 13:14:26 -0600

    checked for null

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,28 @@
 #include "sort.h"
-
-void swap(int* a, int* b)
+/**
+ * swap - Swaps the values of two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ */
+void swap(int *a, int *b)
 {
 	int t = *a;
 	*a = *b;
 	*b = t;
 }
-
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: pointer to the array to be sorted
+ * @size: size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	bool swapped;
+
+	if (array == NULL || size == 0)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
@@ -26,5 +26,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int* a, int* b);
+void swap(int *a, int *b);
 #endif
